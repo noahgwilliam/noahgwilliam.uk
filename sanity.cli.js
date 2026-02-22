0b231d94ae5016c3e2b9444eb960524973f06696
Noah Gwilliam <65233029+noahgwilliam@users.noreply.github.com>
2026-02-23 01:05:48 +0530

    Add Sanity CLI configuration for studio deployment

---
+++ sanity.cli.js
@@ -0,0 +1,9 @@
+import { defineCliConfig } from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: 'z9p5afpf',
+    dataset: 'production'
+  },
+  studioHost: 'noahgwilliam'
+})
